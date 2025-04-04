66e81463ec46d9d7376ab544816d4449fc695a8f
James Conlin <jacon.dev@outlook.com>
2025-04-04 10:59:27 +0100

    minor refactoring following tutorial

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -3,17 +3,20 @@ import { useState } from "react";
 export default function Player({playerName, playerSymbol}) {
     const [isEditing, setIsEditing] = useState(false, )
 
-    function handleEdit() {
+    function handleEditClick() {
         setIsEditing(!isEditing)
     }
 
     return (
         <li>
               <span className="player">
-                  { isEditing ? <input type="text" /> : <span className="player-name">{playerName}</span> }
+                  { isEditing
+                      ? <input type="text" />
+                      : <span className="player-name">{playerName}</span>
+                  }
                 <span className="player-symbol">{playerSymbol}</span>
               </span>
-            <button onClick={handleEdit}>{isEditing ? "Save" : "Edit"}</button>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     )
 }
