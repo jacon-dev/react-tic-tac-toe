dc4d6fb41c26ad0c796579156b62e92be0e68026
James Conlin <jacon.dev@outlook.com>
2025-04-11 15:26:30 +0100

    refactoring out common code

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import GameBoard from "./components/Gameboard/GameBoard.jsx";
 import Log from "./components/Log/Log.jsx";
 import {useState} from "react";
 
+function DeriveActivePlayer(gameTurns){
+    let currentPlayer = 'X'
+    if(gameTurns.length > 0 && gameTurns[0].player === 'X'){
+        currentPlayer = 'O'
+    }
+    return currentPlayer;
+}
+
 function App() {
     const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X')
+    const activePlayer = DeriveActivePlayer(gameTurns);
 
     function handleSelectSquare(rowIndex, colIndex) {
-      setActivePlayer((currentActivePlayer) => (currentActivePlayer === 'X' ? 'O' : 'X'))
         setGameTurns(prevTurns => {
-            let currentPlayer = 'X'
-            if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
-                currentPlayer = 'O'
-            }
+            let currentPlayer = DeriveActivePlayer(prevTurns);
             return [{square: {row: rowIndex, col: colIndex}, player: currentPlayer}, ...prevTurns];
         })
     }
